f241f319d7e87aef5173342b25fcfb0f201b1584
MsDoors.gg <msdoors.gg@gmail.com>
2024-12-31 11:12:11 -0300

    Update Infinite-yield.lua

--- scfiles/Infinite-yield.lua
+++ scfiles/Infinite-yield.lua
@@ -1,4 +1,5 @@
 --[[
 destruído por: https://mscripts.vercel.app/
 ]]--
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Msdoors/mscripts/refs/heads/main/data/lua/Luabanner.lua"))()
 loadstring(game:HttpGet('https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source'))()
