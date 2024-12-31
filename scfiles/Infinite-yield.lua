fbc893ef6f9f17673d9083bf5e38255f756320f3
MsDoors.gg <msdoors.gg@gmail.com>
2024-12-31 01:50:47 -0300

    Update Infinite-yield.lua

--- scfiles/Infinite-yield.lua
+++ scfiles/Infinite-yield.lua
@@ -1,13 +1,4 @@
-loadstring(game:HttpGet('https://raw.githubusercontent.com/SeekerWebS/seekerwebs.github.io/rbsSource/scs-game/dc-seeker.lua'))()
---[[ 
-----------------------------------
-Nome: Infinite Yield
-descrição: Execute comandos admin.
-por; Rhyan57(896604349311115304) - ADMIN
-----------------------------------
-ID: 3
-SITE: https://seekerwebs.github.io/seekerSc.html
-SCRIPT SITE ORIGINAL: https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source
-----------------------------------
+--[[
+destruído por: https://mscripts.vercel.app/
 ]]--
 loadstring(game:HttpGet('https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source'))()
