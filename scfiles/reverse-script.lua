dee84bff0dd83d156e3c38cd2b1194b63b7e46ec
MsDoors.gg <msdoors.gg@gmail.com>
2024-12-31 18:04:46 -0300

    Create reverse-script.lua

---
+++ scfiles/reverse-script.lua
@@ -0,0 +1,43 @@
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/bloodball/-back-ups-for-libs/main/Revenant", true))()
+
+Library.DefaultColor = Color3.fromRGB(0,255,0)
+
+Library:Notification({
+
+    Text = "Fornecido por Mscripts | https://mscripts.vercel.app/",
+
+    Duration = 10
+
+})
+
+Library:Notification({
+
+    Text = "Fornecido por Envixity Scripts no YouTube",
+
+    Duration = 3
+
+})
+
+
+
+wait (2)
+
+
+
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/bloodball/-back-ups-for-libs/main/Revenant", true))()
+
+Library.DefaultColor = Color3.fromRGB(0,255,0)
+
+
+
+Library:Notification({
+
+    Text = "Scripts Envixity no topo! Script executado.",
+
+    Duration = 3
+
+})
+
+
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/0Ben1/fe./main/L"))()
