54cc6167a42790124e0fad8d0d34e05f6138b382
MsDoors.gg <msdoors.gg@gmail.com>
2024-12-31 01:54:20 -0300

    Update mspaint-Rseeker.lua

--- scfiles/mspaint-Rseeker.lua
+++ scfiles/mspaint-Rseeker.lua
@@ -1,14 +1,4 @@
-loadstring(game:HttpGet('https://raw.githubusercontent.com/SeekerWebS/seekerwebs.github.io/rbsSource/scs-game/dc-seeker.lua'))()
---[[ 
-----------------------------------
-Nome: mspaint
-descrição: cópia disfarçada do Mshub
-por; emanuelmiguel(926113693064912896)
-----------------------------------
-ID: 2
-SITE: https://seekerwebs.github.io/seekerSc.html
-SCRIPT SITE ORIGINAL: https://raw.githubusercontent.com/notpoiu/mspaint/main/main.lua
-----------------------------------
+--[[
+destruído por: https://mscripts.vercel.app/
 ]]--
-
 loadstring(game:HttpGet("https://raw.githubusercontent.com/notpoiu/mspaint/main/main.lua"))()
