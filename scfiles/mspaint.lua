a011e8792fd665c1dd2f8b6db3714f652ae28082
MsDoors.gg <msdoors.gg@gmail.com>
2024-12-31 02:41:29 -0300

    Update mspaint.lua

--- scfiles/mspaint.lua
+++ scfiles/mspaint.lua
@@ -1,4 +1,4 @@
 --[[
 destruído por: https://mscripts.vercel.app/
 ]]--
-loadstring(game:HttpGet("https://raw.githubusercontent.com/notpoiu/mspaint/main/main.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Sc-Rhyan57/mspaint/refs/heads/main/main.lua"))()
