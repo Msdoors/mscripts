8d5d380e3e4079140a6a6c5e25e31761437be27b
MsDoors.gg <msdoors.gg@gmail.com>
2025-02-28 13:03:52 -0300

    Update Infinite-yield.lua

--- scfiles/Infinite-yield.lua
+++ scfiles/Infinite-yield.lua
@@ -1,4 +1,4 @@
 --[[
-destruído por: https://mscripts.vercel.app/
+distribuido por: https://mscripts.vercel.app/
 ]]--
 loadstring(game:HttpGet('https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source'))()
